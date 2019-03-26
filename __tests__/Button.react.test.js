f40671c03e49907cc6e349bd7413e627e86e02d4
Igor Kim <igkim90@gmail.com>
2019-03-26 14:03:47 +0500

    add(__tests__) test for button component

---
+++ __tests__/Button.react.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Button } from '../src';
+
+test('component should match snapshot', () => {
+  const component = renderer.create(
+    <Button>Button</Button>,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('component should have "mcs-button-primary" class', () => {
+  const component = renderer.create(
+    <Button type="primary">Button</Button>,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('component should be disabled and have "mcs-button-primary" class', () => {
+  const component = renderer.create(
+    <Button type="primary" disabled>Button</Button>,
+  );
+  const component2 = renderer.create(
+    <Button type="primary" disabled="disabled">Button</Button>,
+  );
+  const component3 = renderer.create(
+    <Button type="primary" disabled={true}>Button</Button>,
+  );
+  const tree = component.toJSON();
+  const tree2 = component2.toJSON();
+  const tree3 = component3.toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(tree2).toMatchSnapshot();
+  expect(tree3).toMatchSnapshot();
+});
